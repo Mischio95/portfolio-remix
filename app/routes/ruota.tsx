8c472f903cea25938ca55751f2a74f2db7c744e6
Mischio95 <65983947+Mischio95@users.noreply.github.com>
2024-12-20 19:45:31 +0100

    fix

--- app/routes/ruota.tsx
+++ app/routes/ruota.tsx
@@ -77,7 +77,7 @@ export default function WheelGame() {
         loop
       />
       <section className="text-center pb-8">
-        <h1 className="text-4xl font-bold animate-zoom ">
+        <h1 className="text-4xl font-bold animate-zoom text-white ">
           Ahhh... la nostalgia!
         </h1>
       </section>
