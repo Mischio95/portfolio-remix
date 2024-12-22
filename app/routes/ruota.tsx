9bb6ef0ab23dbed7f44acbb9b15ade6e85f42747
Mischio95 <65983947+Mischio95@users.noreply.github.com>
2024-12-22 03:04:39 +0100

    modifica il testo della sezione ruota e aggiorna gli stili del dialogo e delle immagini

--- app/routes/ruota.tsx
+++ app/routes/ruota.tsx
@@ -84,7 +84,9 @@ export default function WheelGame() {
           <span>
             Sei nostalgico?
             <br />
-            Gira la ruota della nostalgia!
+            E che c'è di male?
+            <br />
+            NULLA!
           </span>
         </h1>
       </section>
@@ -116,7 +118,7 @@ export default function WheelGame() {
       </div>
 
       <Dialog open={showDialog} onOpenChange={handleDialogOpenChange}>
-        <DialogContent className="border-2 border-[#64ffda] bg-[#10172A] text-white">
+        <DialogContent className="border-2 border-[#ebe5d4] bg-[#10172A] text-white">
           <DialogHeader>
             <DialogTitle className="text-center font-bold uppercase">
               Risultato estrazione:
@@ -127,7 +129,7 @@ export default function WheelGame() {
               <img
                 src="https://www.micheletrombone.it/coupon-calcetto.png"
                 alt="Prize"
-                className="mx-auto border-2 border-[#64ffda] neon-green-shadow rounded-lg"
+                className="mx-auto border-2 border-[#ebe5d4] rounded-lg"
               />
             </div>
             <p className="my-6 text-xl font-bold text-[#dc143c]">
