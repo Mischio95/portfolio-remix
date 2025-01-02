f53dc1f7a847774d24a5e07b16e34853bb9b13ee
Mischio95 <65983947+Mischio95@users.noreply.github.com>
2025-01-02 14:03:08 +0100

    add immage

--- app/components/vertical-carousel.tsx
+++ app/components/vertical-carousel.tsx
@@ -9,29 +9,28 @@ const cards = [
   {
     title: "HACKATHON - MOBILE GAME DEVELOPMENT (BITDROME - MKERS)",
     description: "Sviluppo di una demo di un Mobile Game con Unity.",
-    image:
-      "https://micheletrombone.it/portfolio/gioco1.png?height=300&width=500",
+    image: "https://micheletrombone.it/portfolio/gioco02.png",
     year: "Elo Hell | 20 Ottobre - 10 Novembre 2023",
   },
   {
-    title: "3D",
-    description:
-      "Create immersive three-dimensional worlds that push the boundaries of imagination. Our 3D expertise brings depth and realism to every project.",
-    // image: "/placeholder.svg?height=300&width=500",
-    year: "2021",
+    title: "Destiny's Altered Order - APPLE DEVELOPER ACADEMY",
+    description: "Gioco di ruolo 2D per mobile sviluppato con Unity.",
+    image: "https://micheletrombone.it/portfolio/gioco1.png",
+    year: "Challenge fine anno accademico",
   },
   {
     title: "Motion",
     description:
       "Bring your ideas to life with fluid, dynamic motion graphics. We craft seamless animations that tell your story with style and impact.",
-    // image: "/placeholder.svg?height=300&width=500",
+    image: "https://micheletrombone.it/portfolio/gioco03.png",
     year: "2022",
   },
   {
     title: "Design",
     description:
       "Elevate your visual identity with cutting-edge design solutions. We combine aesthetics with functionality to create memorable experiences.",
-    // image: "/placeholder.svg?height=300&width=500",
+    image:
+      "https://micheletrombone.it/portfolio/gioco3.png?height=300&width=500",
     year: "2023",
   },
 ];
